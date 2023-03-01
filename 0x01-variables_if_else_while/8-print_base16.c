ae528ae85680e4096a5f8cf87f5a45b03ab6f136
Gomo28415 <atkinsongn@gmail.com>
2023-03-01 04:02:09 -0800

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ *main - main block
+ *Description:‘Print all numbers of base 16 in lowercase’
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+		return (0);
+}
