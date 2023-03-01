d1062135aa744974e9a25f32cc5ca3a9dce74574
Gomo28415 <atkinsongn@gmail.com>
2023-03-01 05:26:57 -0800

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+  *main - main block
+  *Description:'prints all possible different combinations of two digits'
+  *Return: 0
+  */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/* elimination repition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/* adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
