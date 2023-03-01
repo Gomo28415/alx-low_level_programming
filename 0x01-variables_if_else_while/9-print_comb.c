d77752825d6523f83206128f87ad127f8ab528a4
Gomo28415 <atkinsongn@gmail.com>
2023-03-01 04:25:22 -0800

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ *main - main block
+ *Description:'Print all combinations of single-digit numbers'
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
